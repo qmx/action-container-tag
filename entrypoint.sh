be82e4b502499720e4c3f45839a33769b02a8fd0
Douglas Campos <qmx@qmx.me>
2019-12-30 13:17:06 +0000

    output docker image name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
+DOCKERHUB_NAMESPACE="$1"
 VERSION="$(git describe --tags --always)"
+IMAGE="$DOCKERHUB_NAMESPACE/$(echo "$GITHUB_REPOSITORY" | cut -f 2 -d/ | sed 's/docker-//')"
 
-echo ::set-output name=version::$VERSION
+echo "::set-output name=image::$IMAGE"
+echo "::set-output name=version::$VERSION"
